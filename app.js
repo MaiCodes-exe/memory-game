2789afe2785bdd5dae67131135817f7a5271e639
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-29 20:33:55 +0100

    added inages folder

---
+++ app.js
@@ -0,0 +1,13 @@
+const cardArray = [
+    {
+    name: 'fries',
+    img: 'images/fries.png'
+},
+{
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png'
+},    {
+    name: 'hotdog',
+    img: 'images/hotdog.png'
+},
+]

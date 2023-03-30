21ade322368a3c936ee5355d839f49d42f349658
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-30 18:31:53 +0100

    finished logic

--- app.js
+++ app.js
@@ -52,7 +52,11 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
-const cardsChosen = []
+const resultDisplay = document.querySelector('#result')
+
+let cardsChosen = []
+let cardsChosenIds =  []
+const cardsWon = []
 
 function createBoard (){
     for (let i = 0 ; i < cardArray.length; i++){
@@ -65,9 +69,50 @@ function createBoard (){
 }
 createBoard()
 
+function checkMatch(){
+    const cards = document.querySelectorAll('#grid img')
+    const optionOne = cardsChosenIds[0]
+    const optionTwo = cardsChosenIds[1]
+    
+    // console.log("check me")
+
+    if (optionOne == optionTwo){
+        cards[optionOne].setAttribute('src', 'images./blank.png');
+        cards[optionTwo].setAttribute('src', 'images./blank.png');
+        alert("You clicked the same card!")
+    }
+
+    if (cardsChosen[0] == cardsChosen[1]){
+        alert("Its a match!")
+        cards[optionOne].setAttribute('src', 'images./white.png');
+        cards[optionTwo].setAttribute('src', 'images./white.png');
+        cards[optionOne].removeEventListener('click', flipCard);
+        cards[optionTwo].removeEventListener('click', flipCard);
+
+        cardsWon.push(cardsChosen)
+    } 
+    else{
+        cards[optionOne].setAttribute('src', 'images./blank.png');
+        cards[optionTwo].setAttribute('src', 'images./blank.png');
+        alert("No match")
+    }
+    resultDisplay.innerHTML = cardsWon.length
+
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if (cardsWon.length == cardArray.length / 2){
+        resultDisplay.innerHTML = 'You won!'
+    }
+}
+
+
 function flipCard(){
     const cardId = this.getAttribute('data')
-    console.log(cardArray[cardId].name)
     cardsChosen.push(cardArray[cardId].name)
-console.log('clicked', cardId)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if(cardsChosen.length === 2){
+    setTimeout(checkMatch, 500)
+    }
 }
